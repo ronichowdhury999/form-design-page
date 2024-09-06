3ac4893d3ed20d5ec4172d5d5a40cf4dee3d6ca8
roniislam999 <mdronii961@gmail.com>
2024-09-06 20:12:19 +0600

    active link option on

--- src/Ui/Header/Header.jsx
+++ src/Ui/Header/Header.jsx
@@ -3,10 +3,13 @@ import {NavLink, } from "react-router-dom"
 const Header = () => {
     return (
         <div>
-            <div className="flex gap-10 py-4 justify-center border font-semibold border-b-2">
-                <NavLink to={'/'}>Login</NavLink>
-                <NavLink to={'loginTwo'}>Login-2</NavLink>
-                <NavLink to={'/home'}>H-About</NavLink>
+            <div className="flex gap-10 py-4  justify-center border font-semibold border-b-2">
+                <NavLink className={({isActive,isPending})=>
+                isPending ? 'pending' : isActive ? 'border-b-2 text-blue-500' : ''} to={'/'}>Login</NavLink>
+                <NavLink className={({isActive,isPending})=>
+                isPending ? 'pending' : isActive ? 'border-b-2 text-blue-500' : ''}  to={'loginTwo'}>Login-2</NavLink>
+                <NavLink className={({isActive,isPending})=>
+                isPending ? 'pending' : isActive ? 'border-b-2 text-blue-500' : ''}  to={'/home'}>H-About</NavLink>
             </div>
         </div>
     )
